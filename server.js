6ae4f4df168579db4106141af3c8daa1910d761d
Bonitas123 <foxdexxdex@gmail.com>
2026-09-15 23:51:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ const DATA_FILE = path.join(__dirname, 'data.json');
 
 function readData() {
   try {
-    if (!fs.existsSync(DATA_FILE)) {
-      const initial = {produtos:[], pedidos:[], config:{taxa:15, endereco:"João Pessoa - PB", pix:"66b5d5fe-b9af-4d26-9f27-2467300dc6e3", zap:"5583999232602", cep:"58000-000"}};
-      fs.writeFileSync(DATA_FILE, JSON.stringify(initial));
-      return initial;
-    }
     return JSON.parse(fs.readFileSync(DATA_FILE,'utf8'));
   } catch(e) {
     return {produtos:[], pedidos:[], config:{taxa:15, endereco:"João Pessoa - PB", pix:"66b5d5fe-b9af-4d26-9f27-2467300dc6e3", zap:"5583999232602", cep:"58000-000"}};
@@ -26,7 +21,6 @@ app.post('/api/dados', (req,res)=>{
   fs.writeFileSync(DATA_FILE, JSON.stringify(req.body, null, 2)); 
   res.json({ok:true}); 
 });
-
 app.get('*', (req,res)=>{ res.sendFile(path.join(__dirname, 'index.html')); });
 
 const PORT = process.env.PORT || 3000;
